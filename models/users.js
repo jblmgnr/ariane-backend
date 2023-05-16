730c7b909e780b3665ab7771c1de43513bc7bb24
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-05-16 09:19:50 +0200

    add clé étrangère tree au user

--- models/users.js
+++ models/users.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    token: String,
-    role: [String],
-    autoLogin: Boolean,
-    member:{type: mongoose.Schema.Types.ObjectId, ref: 'members'}
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  token: String,
+  role: [String],
+  autoLogin: Boolean,
+  member: { type: mongoose.Schema.Types.ObjectId, ref: "members" },
+  tree: { type: mongoose.Schema.Types.ObjectId, ref: "trees" },
 });
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
